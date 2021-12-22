43b2a3d1bb67edfb0c9fca31cc3562cc3153ef5d
dohung <dohuyhung12@gmail.com>
2021-12-22 14:52:31 +0700

    add input mode for form and mode controller

--- src/form.js
+++ src/form.js
@@ -1,13 +1,16 @@
 import { todoList } from "./index";
 import { Todo } from "./todo-object";
 import { displayTodoList } from "./displayControl";
+
+let editFlag = false;
+let editID = "";
 function createForm() {
   const form = document.createElement("form");
   form.className = "todo-input";
 
-  const toDoFieldset = createTodoInputFieldset();
+  const toDoFieldset = createTodoInput();
 
-  const newProjectFieldset = createAddProjectFieldSet();
+  const newProjectFieldset = createAddProject();
   toDoFieldset.appendChild(newProjectFieldset);
 
   form.appendChild(toDoFieldset);
@@ -15,33 +18,65 @@ function createForm() {
   const btn = createSubmitBtn("todo-input");
   form.appendChild(btn);
 
-  form.addEventListener("submit", submitHandler);
+  form.addEventListener("submit", addItem);
   return form;
 }
 
-function submitHandler(e) {
+function setEditDetails(editFlagVal, editIDVal, btnText) {
+  const btn = document.querySelector(".todo-input-btn");
+  editFlag = editFlagVal;
+  editID = editIDVal;
+  btn.textContent = btnText;
+}
+function addItem(e) {
   e.preventDefault();
 
-  let props = [];
+  const name = document.querySelector("#name").value;
+  const id = new Date().getTime().toString();
+  if (name !== "" && !editFlag) {
+    let props = [id];
+    const fields = ["name", "description", "due-date", "priority", "project"];
+    for (let field of fields) {
+      props.push(document.querySelector(`#${field}`).value);
+    }
 
-  const fields = ["name", "description", "due-date", "priority", "project"];
-  for (let field of fields) {
-    props.push(document.querySelector(`#${field}`).value);
+    const todo = new Todo(
+      props[0],
+      props[1],
+      props[2],
+      props[3],
+      props[4],
+      props[5]
+    );
+    todoList.push(todo);
+    e.currentTarget.reset();
+
+    const content = document.querySelector("#content");
+    displayTodoList(todoList, content);
+  } else if (name !== "" && editFlag) {
+    // get edit obj by the ID
+    const todo = todoList.filter((item) => (item.id = editID))[0];
+    const todoDisplay = document.querySelector(`[data-id="${editID}" ]`);
+
+    const props = ["name", "description", "due-date", "priority", "project"];
+
+    for (let prop of props) {
+      const value = document.querySelector(`#${prop}`).value;
+
+      todo[prop] = value;
+      todoDisplay.querySelector(`.${prop}`).textContent = value;
+    }
+    // reset form back to input mode
+    setEditDetails(false, "", "add");
+    e.currentTarget.reset();
   }
-
-  const todo = new Todo(props[0], props[1], props[2], props[3], props[4]);
-  todoList.push(todo);
-  e.currentTarget.reset();
-
-  const content = document.querySelector("#content");
-  displayTodoList(todoList, content);
 }
 
-function createAddProjectFieldSet() {
+function createAddProject() {
   const fieldset = document.createElement("fieldset");
   const legend = document.createElement("label");
   legend.textContent = "Add new project";
-  addInputField("text", "add-project", fieldset);
+  addInput("text", "add-project", fieldset);
   const btn = createSubmitBtn("add-project");
   btn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -68,23 +103,23 @@ function createAddProjectFieldSet() {
   return fieldset;
 }
 
-function createTodoInputFieldset() {
+function createTodoInput() {
   const project = ["personal", "work"];
   const fieldset = document.createElement("fieldset");
   const legend = document.createElement("legend");
   legend.textContent = "Create your Todo";
   fieldset.appendChild(legend);
 
-  addInputField("text", "name", fieldset);
-  addInputField("text", "description", fieldset);
-  addInputField("date", "due-date", fieldset);
-  addOptionField("priority", ["low", "normal", "high"], fieldset);
-  addOptionField("project", project, fieldset);
+  addInput("text", "name", fieldset);
+  addInput("text", "description", fieldset);
+  addInput("date", "due-date", fieldset);
+  addOptionTypeInput("priority", ["low", "normal", "high"], fieldset);
+  addOptionTypeInput("project", project, fieldset);
 
   return fieldset;
 }
 
-function addInputField(type, name, form) {
+function addInput(type, name, form) {
   const input = document.createElement("input");
   const label = document.createElement("label");
 
@@ -99,7 +134,7 @@ function addInputField(type, name, form) {
   form.appendChild(input);
 }
 
-function addOptionField(id, items, form) {
+function addOptionTypeInput(id, items, form) {
   function createOption(value) {
     const option = document.createElement("option");
     option.setAttribute("value", value);
@@ -131,4 +166,4 @@ function createSubmitBtn(name) {
   return btn;
 }
 
-export { createForm };
+export { createForm, setEditDetails };
