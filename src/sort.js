994760039abc648b035aebbdb8fbcd49fddd8bcc
dohung <dohuyhung12@gmail.com>
2021-12-23 19:02:07 +0700

    change from named export to default export

--- src/sort.js
+++ src/sort.js
@@ -1,7 +1,35 @@
 import { displayTodoList } from "./displayControl";
 import { todoList } from "./index";
 
-function createSort() {
+function sortClickHandler(e) {
+  e.preventDefault();
+  const sort = document.querySelector("#sort");
+
+  const project = sort.value;
+  if (project !== "all") {
+    const newList = todoList.filter((todo) => todo._project === project);
+    displayTodoList(newList);
+  } else {
+    displayTodoList(todoList);
+  }
+}
+
+function createSortOption(sort) {
+  const allOption = document.createElement("option");
+  allOption.setAttribute("value", "all");
+  allOption.textContent = "all";
+  sort.add(allOption);
+
+  const project = document.querySelector("#project");
+  const options = project.querySelectorAll("option");
+
+  options.forEach((option) => {
+    const newOption = document.importNode(option, true);
+    sort.appendChild(newOption);
+  });
+}
+
+export default function createSort() {
   const div = document.createElement("div");
   div.className = "sort-container";
   const fieldset = document.createElement("fieldset");
@@ -24,33 +52,3 @@ function createSort() {
   div.appendChild(fieldset);
   return div;
 }
-
-function sortClickHandler(e) {
-  e.preventDefault();
-  const sort = document.querySelector("#sort");
-
-  const project = sort.value;
-  if (project !== "all") {
-    let newList = todoList.filter((todo) => todo._project === project);
-    displayTodoList(newList);
-  } else {
-    displayTodoList(todoList);
-  }
-}
-
-function createSortOption(sort) {
-  const allOption = document.createElement("option");
-  allOption.setAttribute("value", "all");
-  allOption.textContent = "all";
-  sort.add(allOption);
-
-  const project = document.querySelector("#project");
-  const options = project.querySelectorAll("option");
-
-  for (let option of options) {
-    const newOption = document.importNode(option, true);
-    sort.appendChild(newOption);
-  }
-}
-
-export { createSort };
